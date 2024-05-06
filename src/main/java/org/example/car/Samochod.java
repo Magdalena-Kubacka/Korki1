6447ff820399be480b3b5e7517d51b20b2c9d411
Bartek Dziadosz <bdziadosz96@icloud.com>
2024-05-06 13:27:12 +0200

    Zadanie 1

---
+++ src/main/java/org/example/car/Samochod.java
@@ -0,0 +1,11 @@
+package org.example.car;
+
+public class Samochod {
+    //nazwa
+    //ilosc paliwa wyrazona w 0-100
+    //spalanie paliwa na 100km
+
+    //metoda jedz(Integer km) {}
+
+    //zmien nazwe(String nazwa) {}
+}
---
+++ src/main/java/org/example/car/SamochodMain.java
@@ -0,0 +1,10 @@
+package org.example.car;
+
+public class SamochodMain {
+    public static void main(String[] args) {
+        //utworzenie samochodu
+
+
+        //przejechanie dystansu 10,50,100 km
+    }
+}
