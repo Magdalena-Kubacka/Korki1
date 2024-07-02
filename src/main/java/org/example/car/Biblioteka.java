43976fa0ddcec627e9a2360c0e5d2aee7ec79794
Bartek Dziadosz <bdziadosz96@icloud.com>
2024-07-02 09:32:24 +0200

    Zadanie 2 / 3

--- src/main/java/org/example/car/Biblioteka.java
+++ src/main/java/org/example/car/Biblioteka.java
@@ -1,36 +1,97 @@
 package org.example.car;
-  /*(1) Biblioteka
-          Opis:
-          Napisz program do zarządzania biblioteką. Użyj klasy Ksiazka,
-          aby przechowywać informacje o każdej książce (tytuł, autor, rok wydania).
-          Program powinien umożliwiać dodawanie książek do biblioteki, wyświetlanie wszystkich książek oraz wyszukiwanie książek po tytule.
 
-          Szczegóły:
-          Stwórz klasę Ksiazka z polami String tytul, String autor, int rokWydania.
-          Stwórz klasę Biblioteka, która będzie przechowywać listę książek w tablicy lub kolekcji.
-          Dodaj metody do klasy Biblioteka takie jak dodajKsiazke(Ksiazka k), wyszukajKsiazke(String tytul), wyswietlKsiazki().
-          Użyj pętli do iteracji przez listę książek w metodach wyszukajKsiazke i wyswietlKsiazki.
-          */
+/*(1) Biblioteka
+        Opis:
+        Napisz program do zarządzania biblioteką. Użyj klasy Ksiazka,
+        aby przechowywać informacje o każdej książce (tytuł, autor, rok wydania).
+        Program powinien umożliwiać dodawanie książek do biblioteki, wyświetlanie wszystkich książek oraz wyszukiwanie książek po tytule.
+
+        Szczegóły:
+        Stwórz klasę Ksiazka z polami String tytul, String autor, int rokWydania.
+        Stwórz klasę Biblioteka, która będzie przechowywać listę książek w tablicy lub kolekcji.
+        Dodaj metody do klasy Biblioteka takie jak dodajKsiazke(Ksiazka k), wyszukajKsiazke(String tytul), wyswietlKsiazki().
+        Użyj pętli do iteracji przez listę książek w metodach wyszukajKsiazke i wyswietlKsiazki.
+        */
 public class Biblioteka {
-      public static void main(String[] args) {
-          Książka[] książki = new Książka[10];
+    public static void main(String[] args) {
+        Książka[] książki = new Książka[4];
 
-          książki[0] = new Książka("Java Podstawy", "Cay S. Horstmann", 2021);
-          książki[1] = new Książka("Japonia", "Phillip Tang", 2022);
-          książki[2] = new Książka("Git i GitHub", "Mariot Tsitoara", 2020);
-
-
-          System.out.println(książki[0]);
+        książki[0] = new Książka("Java Podstawy", "Cay S. Horstmann", 2021);
+        książki[1] = new Książka("Japonia", "Phillip Tang", 2022);
+        książki[2] = new Książka("Git i GitHub", "Mariot Tsitoara", 2020);
 
 
-          for (Książka wyświetl : książki) {
-              System.out.println(wyświetl);
-          }
+        int miejsce = 3;
+        String nowaKsiazkaTytul = "Typowa Ksiazka";
+        String nowaKsiazkaAutor = "Typowy Autor";
+        int nowaKsiazkaRokWydania = 2030;
+
+        dodajKsiazke(książki, miejsce, nowaKsiazkaTytul, nowaKsiazkaAutor, nowaKsiazkaRokWydania);
 
 
-      }
+//        for (Książka wyświetl : książki) {
+//            System.out.println(wyświetl);
+//        }
 
-  }
+//        for (int i = 0; i < książki.length; i++) {
+//            System.out.println(książki[i]);
+//        }
+
+//        String tytulWyszukiwanejKsiazki = "Git i GitHub";
+//        wyszukajKsiazke(książki, tytulWyszukiwanejKsiazki);
+
+        String nowyAutor = "Przykladowy Autor 2024";
+        int szukanyRokWydania = 2022;
+        aktualizuj(książki, szukanyRokWydania, nowyAutor);
+
+        for (Książka wyświetl : książki) {
+            System.out.println(wyświetl);
+        }
+        
+    }
+
+    private static void wyszukajKsiazke(Książka[] książki,
+                                        String tytulWyszukiwanejKsiazki) {
+        for (Książka książka : książki) {
+            if (książka.getTytul().equals(tytulWyszukiwanejKsiazki)) {
+                System.out.println("Znaleziono ksiazke " + książka);
+            }
+        }
+    }
+
+    private static void dodajKsiazke(Książka[] ksiazki,
+                                     int miejsce,
+                                     String nowaKsiazkaTytul,
+                                     String nowaKsiazkaAutor,
+                                     int nowaKsiazkaRokWydania) {
+        Książka książka = new Książka(nowaKsiazkaTytul, nowaKsiazkaAutor, nowaKsiazkaRokWydania);
+        ksiazki[miejsce] = książka;
+    }
+
+    private static void aktualizuj(Książka[] ksiazki,
+                                   int rokWydania,
+                                   String nowyAutor) {
+        for (Książka książka : ksiazki) {
+            if (książka.getRokWydania() == rokWydania) {
+                książka.setAutor(nowyAutor);
+            }
+        }
+    }
+
+    private static void aktualizuj2(Książka[] ksiazki,
+                                   int rokWydania,
+                                   String nowyAutor) {
+        for (int i = 0; i < ksiazki.length; i++) {
+            if (ksiazki[i].getRokWydania() == rokWydania) {
+                Książka książka = ksiazki[i];
+                Książka książka1 = new Książka(książka.getTytul(), nowyAutor, książka.getRokWydania());
+                ksiazki[i] = null;
+                ksiazki[i] = książka1;
+            }
+        }
+    }
+
+}
 
 
 /*  (1) Bilioteka v2
--- src/main/java/org/example/car/Książka.java
+++ src/main/java/org/example/car/Książka.java
@@ -1,22 +1,43 @@
 package org.example.car;
+
 /*Dodaj metody do klasy Biblioteka takie jak dodajKsiazke(Ksiazka k), wyszukajKsiazke(String tytul), wyswietlKsiazki().
         Użyj pętli do iteracji przez listę książek w metodach wyszukajKsiazke i wyswietlKsiazki.*/
-public class Książka {
+public class Książka extends Object{
     private String tytuł;
     private String autor;
     private Integer rokWydania;
 
-    public Książka(String tytuł, String autor, Integer rokWydania) {
+    public Książka(String tytuł,
+                   String autor,
+                   Integer rokWydania) {
         this.tytuł = tytuł;
         this.autor = autor;
         this.rokWydania = rokWydania;
     }
-    public String wyszukajKsiążkę (){
-        return tytuł;
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTytul() {
+        return this.tytuł;
+    }
+
+    public Integer getRokWydania() {
+        return rokWydania;
+    }
+
+    @Override
+    public String toString() {
+        return "Książka{" +
+                       "tytuł='" + tytuł + '\'' +
+                       ", autor='" + autor + '\'' +
+                       ", rokWydania=" + rokWydania +
+                       '}';
     }
 
 
-    }
+}
 
 
 
